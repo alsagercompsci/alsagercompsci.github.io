33469039f335bff05567ae55700f868d6749496a
Tyler <144330871+tbperson@users.noreply.github.com>
2024-11-28 10:19:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.querySelector('#about1 img[src="images/python.png"]').addEventListener('click', function() {
+document.querySelector('#about_alternative img[src="images/python.png"]').addEventListener('click', function() {
     window.location.href = 'https://www.w3schools.com/python/';
 });
-document.querySelector('#about1 img[src="images/javascript.png"]').addEventListener('click', function() {
+document.querySelector('#about_alternative img[src="images/javascript.png"]').addEventListener('click', function() {
     window.location.href = 'https://www.w3schools.com/js/';
 });
-document.querySelector('#about1 img[src="images/htmlcss.png"]').addEventListener('click', function() {
+document.querySelector('#about_alternative img[src="images/htmlcss.png"]').addEventListener('click', function() {
     window.location.href = 'https://www.w3schools.com/html/';
 });
-document.querySelector('#about3 img[src="images/cs50p.png"]').addEventListener('click', function() {
+document.querySelector('#about_alternative img[src="images/cs50p.png"]').addEventListener('click', function() {
     window.location.href = 'https://cs50.harvard.edu';
 });
-document.querySelector('#about3 img[src="images/highseas.png"]').addEventListener('click', function() {
+document.querySelector('#about_alternative img[src="images/highseas.png"]').addEventListener('click', function() {
     window.location.href = 'https://hackclub.com/';
 });
-document.querySelector('#about5 img[src="images/github.png"]').addEventListener('click', function() {
+document.querySelector('#about_alternative img[src="images/github.png"]').addEventListener('click', function() {
     window.location.href = 'https://guides.github.com/activities/hello-world/';
 });
 
