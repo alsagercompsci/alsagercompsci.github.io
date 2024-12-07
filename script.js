71ff8480a21349d4415f77cb2045fbd3442e16ba
Tyler <144330871+tbperson@users.noreply.github.com>
2024-12-07 11:20:26 +0000

    Changed the querrySelector in script.js to listen for images in the "about" div, and removed calls for script.js in all files apart from about.html

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-document.querySelector('#about_alternative img[src="images/python.png"]').addEventListener('click', function() {
+document.querySelector('#about img[src="images/python.png"]').addEventListener('click', function() {
     window.location.href = 'https://www.w3schools.com/python/';
 });
-document.querySelector('#about_alternative img[src="images/javascript.png"]').addEventListener('click', function() {
+document.querySelector('#about img[src="images/javascript.png"]').addEventListener('click', function() {
     window.location.href = 'https://www.w3schools.com/js/';
 });
-document.querySelector('#about_alternative img[src="images/htmlcss.png"]').addEventListener('click', function() {
+document.querySelector('#about img[src="images/htmlcss.png"]').addEventListener('click', function() {
     window.location.href = 'https://www.w3schools.com/html/';
 });
-document.querySelector('#about_alternative img[src="images/cs50p.png"]').addEventListener('click', function() {
+document.querySelector('#about img[src="images/cs50p.png"]').addEventListener('click', function() {
     window.location.href = 'https://cs50.harvard.edu';
 });
-document.querySelector('#about_alternative img[src="images/highseas.png"]').addEventListener('click', function() {
+document.querySelector('#about img[src="images/highseas.png"]').addEventListener('click', function() {
     window.location.href = 'https://hackclub.com/';
 });
 
