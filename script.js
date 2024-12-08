5f67d93801d9e4a22d30ac18bc85a21ae512ce83
Tyler <144330871+tbperson@users.noreply.github.com>
2024-12-08 22:12:18 +0000

    Comments on script.js for future devs

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//This script is used to redirect users to educational sites when they click on the images in the about section.
+//It is reccomended to update broken or old links to the latest version of the site.
+//This script is only used in the about section of the website.
+//If you add images anywhere in about.html, make sure to add the event listener for the image in this script.
+//If you add them elsewhere in the site, adjust this file with an if clause to detect which file they are on, or create a new script file for that page.
 document.querySelector('#about img[src="images/python.png"]').addEventListener('click', function() {
     window.location.href = 'https://www.w3schools.com/python/';
 });
